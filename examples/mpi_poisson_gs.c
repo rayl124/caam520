83c828e436f9a9c561830805dfa435d9d89ae4ca
Christopher Thiele <ct37@rice.edu>
2020-02-10 18:42:14 -0600

    Added MPI-based Gauss-Seidel example.

--- examples/mpi_poisson_gs.c
+++ examples/mpi_poisson_gs.c
@@ -1,5 +1,5 @@
-// mpi_poisson_jacobi.c - Solve Poisson's equation using finite differences
-//                        and the Jacobi method.
+// mpi_poisson_gs.c - Solve Poisson's equation using finite differences
+//                    and a wave front Gauss-Seidel method.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,28 +25,11 @@ void exchange_halo(double *u, int n, int n_local)
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  // Get pointers to the upper and lower ghost layers.
-  double *lower = u;
+  // Get pointer to the upper ghost layer.
   double *upper = &u[n*n*(n_local + 1)];
-  // Get pointers to the top and bottom layer of this rank's slice of the grid,
-  // i.e., the first and last layer that is *not* a ghost layer.
+  // Get pointer to the bottom layer of this rank's slice of the grid,
+  // i.e., the first layer that is *not* a ghost layer.
   double *bottom = &u[n*n];
-  double *top = &u[n*n*n_local];
-
-  // Send top layer to next rank, receive lower ghost layer from previous rank.
-  // Rank zero only sends data, while the last rank only receives data.
-  if (rank == 0) {
-    MPI_Send(top, n*n, MPI_DOUBLE, 1, 999, MPI_COMM_WORLD);
-  }
-  else if (rank == size - 1) {
-    MPI_Recv(lower, n*n, MPI_DOUBLE, size - 2, 999,
-             MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  }
-  else {
-    MPI_Sendrecv(top, n*n, MPI_DOUBLE, rank + 1, 999,
-                 lower, n*n, MPI_DOUBLE, rank - 1, 999,
-                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  }
 
   // Send bottom layer to previous rank, receive upper ghost layer from next
   // rank.
@@ -64,58 +47,85 @@ void exchange_halo(double *u, int n, int n_local)
   }
 }
 
-void jacobi_update(double *u_new,
-                   double *u_old,
-                   const double *f,
-                   int n,
-                   int n_local)
+void gauss_seidel_update(double *u,
+                         const double *f,
+                         int n,
+                         int n_local,
+                         int block_size)
 {
   const double h = 1.0/(n + 1);
   const double h2 = h*h;
 
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
   // Perform a halo exchange, i.e., get ghost layer values from neighboring
   // ranks.
-  exchange_halo(u_new, n, n_local);
+  exchange_halo(u, n, n_local);
 
-  // Copy u_new to u_old.
-  memcpy(u_old, u_new, n*n*(n_local + 2)*sizeof(double));
+  // Get pointers to the lower ghost layer and the top layer of this rank's
+  // slice of the grid.
+  double *lower = u;
+  double *top = &u[n*n*n_local];
 
-  // Perform the Jacobi update.
-  for (int k_local = 0; k_local < n_local; k_local++) {
-    for (int j = 0; j < n; j++) {
+  // Perform the Gauss-Seidel update.
+  for (int j = 0; j < n; j++) {
+    if (j%block_size == 0 && rank > 0) {
+      // We are entering a new block in j-direction.
+      // Get the ghost layer values from the previous rank.
+      MPI_Recv(&lower[n*j], n*block_size, MPI_DOUBLE,
+               rank - 1, 999, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+
+    for (int k_local = 0; k_local < n_local; k_local++) {
       for (int i = 0; i < n; i++) {
         // Compute the index of the current grid point, offset by the ghost
         // layer size (n x n).
         const int idx = n*n*k_local + n*j + i + n*n;
 
         double u_ijk = h2*f[idx];
-        if (i > 0)     u_ijk += u_old[idx - 1];
-        if (i < n - 1) u_ijk += u_old[idx + 1];
-        if (j > 0)     u_ijk += u_old[idx - n];
-        if (j < n - 1) u_ijk += u_old[idx + n];
+        if (i > 0)     u_ijk += u[idx - 1];
+        if (i < n - 1) u_ijk += u[idx + 1];
+        if (j > 0)     u_ijk += u[idx - n];
+        if (j < n - 1) u_ijk += u[idx + n];
         // Thanks to the ghost layers, there is always a previous element and 
         // a next element in k-direction.
-        u_ijk += u_old[idx - n*n];
-        u_ijk += u_old[idx + n*n];
+        u_ijk += u[idx - n*n];
+        u_ijk += u[idx + n*n];
 
         u_ijk /= 6.0;
-        u_new[idx] = u_ijk;
+        u[idx] = u_ijk;
       }
+    }
+
+    if (j%block_size == block_size - 1 && rank < size - 1) {
+      // We finished a block in j-direction.
+      // Send updated boundary values to next rank.
+      MPI_Send(&top[n*(j - block_size + 1)], n*block_size, MPI_DOUBLE,
+               rank + 1, 999, MPI_COMM_WORLD);
     }
   }
 }
 
 int main(int argc, char **argv)
 {
-  // Get problem size and number of iterations.
-  if (argc != 3) {
-    fprintf(stderr, "Usage: mpirun -n N ./mpi_poisson_jacobi n num_iter\n");
+  // Get problem size, block size, and number of iterations.
+  if (argc != 4) {
+    fprintf(stderr,
+            "Usage: mpirun -n N ./mpi_poisson_gs n block_size num_iter\n");
     return -1;
   }
   const int n = atoi(argv[1]);
-  const int num_iter = atoi(argv[2]);
+  const int block_size = atoi(argv[2]);
+  const int num_iter = atoi(argv[3]);
 
   const double h = 1.0/(n + 1);
+
+  if (n%block_size != 0) {
+    fprintf(stderr, "Error: Block size must divide the problem size!\n");
+    return -1;
+  }
 
   MPI_Init(&argc, &argv);
 
@@ -142,15 +152,12 @@ int main(int argc, char **argv)
   }
 
 
-  // Allocate arrays for the new and old solution as well as the right-hand
-  // side.
+  // Allocate arrays for the solution as well as the right-hand side.
   // Include "ghost layers."
-  double *u_new = malloc(n*n*(n_local + 2)*sizeof(double));
-  double *u_old = malloc(n*n*(n_local + 2)*sizeof(double));
+  double *u = malloc(n*n*(n_local + 2)*sizeof(double));
   double *f = malloc(n*n*(n_local + 2)*sizeof(double));
-  if (!u_new || !u_old || !f) {
-    free(u_new);
-    free(u_old);
+  if (!u || !f) {
+    free(u);
     free(f);
     fprintf(stderr, "Error: Could not allocate memory!\n");
     MPI_Finalize();
@@ -174,7 +181,7 @@ int main(int argc, char **argv)
         const int idx = n*n*k_local + n*j + i + n*n;
 
         // Set initial guess.
-        u_old[idx] = 0.0;
+        u[idx] = 0.0;
 
         // Set right-hand side.
         f[idx] = f_rhs(x, y, z);
@@ -192,14 +199,14 @@ int main(int argc, char **argv)
       const int idx_upper = n*n*(n_local + 1) + n*j + i;
 
       // Set values.
-      u_old[idx_lower] = 0.0;
-      u_old[idx_upper] = 0.0;
+      u[idx_lower] = 0.0;
+      u[idx_upper] = 0.0;
     }
   }
 
-  // Perform Jacobi iteration.
+  // Perform Gauss-Seidel iteration.
   for (int iter = 0; iter < num_iter; iter++) {
-    jacobi_update(u_new, u_old, f, n, n_local);
+    gauss_seidel_update(u, f, n, n_local, block_size);
   }
 
   // Compute relative error.
@@ -224,7 +231,7 @@ int main(int argc, char **argv)
         const int idx = n*n*k_local + n*j + i + n*n;
 
         // Compute error norm at the current grid point.
-        const double diff = u_new[idx] - u_exact_ijk;
+        const double diff = u[idx] - u_exact_ijk;
         norm_error_local += diff*diff;
         norm_u_exact_local += u_exact_ijk*u_exact_ijk;
       }
@@ -246,8 +253,7 @@ int main(int argc, char **argv)
   }
 
   MPI_Finalize();
-  free(u_new);
-  free(u_old);
+  free(u);
   free(f);
   return 0;
 }
